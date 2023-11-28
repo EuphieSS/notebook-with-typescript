e32bf2b38a153c001134c65b90c4d907739fb581
--replace-all <samma118@hotmail.com>
2023-11-27 21:49:51 -0500

    Add logic to auto populate edit form

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -9,13 +9,21 @@ type NoteFormProps = {
   onSubmit: (data: NoteData) => void
   onAddTag: (tag: Tag) => void
   availableTags: Tag[]
-};
+} & Partial<NoteData>;
+// Partial means all NoteData is passed in, but none are required
 
-export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) { 
+export function NoteForm({
+  onSubmit,
+  onAddTag,
+  availableTags,
+  title = "",
+  markdown = "",
+  tags = []
+}: NoteFormProps) { 
   // above uses destructuring to extract the onSubmit function from the props object passed to the component 
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
-  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>(tags);
   const navigate = useNavigate();
 
   function handleSubmit(e: FormEvent) {
@@ -37,7 +45,7 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
           <Col>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control ref={titleRef} required />
+              <Form.Control ref={titleRef} required defaultValue={title}/>
             </Form.Group>
           </Col>
 
@@ -71,7 +79,7 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
 
         <Form.Group controlId="markdown">
           <Form.Label>Body</Form.Label>
-          <Form.Control required as="textarea" ref={markdownRef} rows={15}/>
+          <Form.Control required as="textarea" ref={markdownRef} defaultValue={markdown} rows={15}/>
         </Form.Group>
 
         <Stack direction="horizontal" gap={2} className="justify-content-end">
